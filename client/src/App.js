b95015ec9f9f4899eaaa29a9cdaea96bb2eb6c91
Wes White <wtealwhite@gmail.com>
2019-02-07 21:52:56 -0600

    worked on input

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,33 @@ import './App.css';
 class App extends Component {
   constructor(){
     super()
+    this.state = {msg: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({msg: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.msg);
+    event.preventDefault();
   }
 
   componentDidMount(){
     const uri = 'ws://' + 'localhost:3030' + '/chat';
-    const ws = new WebSocket(uri)
+    var ws = new WebSocket(uri)
 
-    //ws.onmessage(msg => console.log(msg))
+    var msg = {
+      type: "message",
+      text: "test",
+      id:   1,
+      date: Date.now()
+    };
+    //ws.onopen( event => ws.send(msg))
+
   }
 
   render() {
@@ -19,7 +39,10 @@ class App extends Component {
       <div className="App">
           <div className="msg-box">
           </div>
-          <input className="msg" />
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.msg} onChange={this.handleChange}/>
+            <button type="submit">SEND</button>
+          </form>
       </div>
     );
   }
